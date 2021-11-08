f253107caab8c12464c9b67d9488801a64c31a03
shi xing liu <garyabc0123@gmail.com>
2021-11-08 15:06:08 +0800

    add commit add position.go struct

--- nToken/nToken.go
+++ nToken/nToken.go
@@ -17,20 +17,29 @@ type TokenStream struct {
 func operatorPriority(in SymbolTable) int {
 	switch in {
 	case squareBracketLeft, squareBracketRight, curlyBracketLeft, curlyBracketRight:
+		//[ ] { }
 		return 3
 	case dollerSign, percentSign:
+		//$ %
 		return 4
 	case exclamationMark:
+		//!
 		return 5
 	case caret:
+		//^
 		return 6
 	case verticalBar:
+		//|
 		return 7
 	default:
 		return 100
 	}
 }
 
+/**
+ * parse tree data struct
+ * if only has one child, must put into Left leaf node
+ */
 type ParseTree struct {
 	Token TokenStream
 	Left  *ParseTree
@@ -39,7 +48,19 @@ type ParseTree struct {
 
 /**
  * Symbol List
- */
+
+| 符號 |                說明                 | 優先度(1是最高) |
+| ---- |:----------------------------------:|:---------------:|
+| \    |   將下一個字元標記為一個特殊字元         |        1        |
+| $    |       下一個字元開始為查詢詞           |        4        |
+| %    |      下一個字元開始為查詢詞性          |        4        |
+| \|   |             or                     |       7        |
+| !    |                not                 |       5        |
+| ^    |                and                 |       6        |
+| {}[] | `{}`裡面是查詢運算，`[]`裡面是間距      |      2         |
+| {}   |  括號                               |      3         |
+
+*/
 type SymbolTable byte
 
 const (
@@ -54,11 +75,21 @@ const (
 	curlyBracketRight  = '}'
 )
 
+/**
+ * input data struct
+ * must be:
+ * word(part of speeh id) word(part of speeh id) word(part of speeh id)...
+ */
 type WordAndPartOfSpeechPair struct {
-	Id          int
-	Word        []rune
-	PartOfSeech int
+	Id           int
+	Word         []rune
+	PartOfSpeech int
 }
 
 type Sentence []*WordAndPartOfSpeechPair
 type Document []*Sentence
+
+//position data struct
+type Position int
+type SentencePositionList []Position
+type DocumentPositionList []Sentence
